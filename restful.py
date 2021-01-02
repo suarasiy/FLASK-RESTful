7b6de754aff81332052cf897c2728d2ea112a9f9
suarasiy <sidiqindrajatiyusuf@gmail.com>
2021-01-02 09:47:35 +0800

    Authentication, reqparse

--- restful.py
+++ restful.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -13,6 +13,14 @@ jwt = JWT(app, authenticate, identity) # create new endpoint /auth
 items = []
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        "price",
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x["name"] == name, items), None)
@@ -22,13 +30,32 @@ class Item(Resource):
         if next(filter(lambda x: x["name"] == name, items), None):
             return {"message" : f"An item with name {name} already exists."}, 400
 
-        data_post = request.get_json()
+        data_post = Item.parser.parse_args()
+
         item = {
             "name" : name,
             "price" : data_post["price"]
             }
         items.append(item)
         return item, 201
+    
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x["name"] != name, items))
+        return {"message" : "Item deleted."}
+
+    def put(self, name):
+        data_put = Item.parser.parse_args()
+        item = next(filter(lambda x: x["name"] == name, items), None)
+        if item is None:
+            item = {
+                "name" : name,
+                "price" : data_put["price"]
+            }
+            items.append(item)
+        else:
+            item.update(data_put)
+        return item
 
 class ItemList(Resource):
     def get(self):
